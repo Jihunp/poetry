e746528f419e3fb8f986f9082196f1aa7408ae9b
Jihunp <edwardpark.park@gmail.com>
2022-09-21 17:42:39 -0700

    created new scripts and server listener

---
+++ poems-backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+//DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+
+//express application
+const app = express();
+
+//port env
+const PORT = process.env.PORT;
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//ROUTES
+// const poemRouter = require("./routes/menu");
+// app.use("/poem", poemRouter);
+
+//LISTEN
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
