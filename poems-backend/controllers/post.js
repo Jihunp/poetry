647ba173fdb749cd442943a9e1d59cae94fbbb6a
Jihunp <edwardpark.park@gmail.com>
2022-09-26 22:21:46 -0700

    created routes and controllers

--- poems-backend/controllers/post.js
+++ poems-backend/controllers/post.js
@@ -15,14 +15,72 @@ const index = (req, res) => {
     });
 };
 
-// async function index(req, res) {
-//     try{
-//         res.json(await Post.find({}));
-//     } catch (error) {
-//         res.status(400).json(error);
-//     }
-// }
+const show = (req, res) => {
+    db.Post.findbyId(req.params.id, (err, foundPost) => {
+        if (err)
+            return res.status(400).json({
+                message: "Status 400 Error!",
+                error: err,
+            });
+        return res.status(200).json({
+            message: "Success!",
+            data: foundPost,
+        });
+    });
+};
+
+const create = (req, res) => {
+    db.Post.create(req.body, (err, savedPost) => {
+        if (err)
+            return res.status(400).json({
+                message: "Status 400 Error!",
+                error: err,
+            });
+        return res.status(200).json({
+            message: "Success!",
+            data: savedPost,
+        });
+    });
+};
+
+const update = (req, res) => {
+    db.Post.findbyIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedPost) => {
+            if (err)
+                return res.status(400).json({
+                    message: "Status 400 Error",
+                    error: err,
+                });
+            return res.status(200).json({
+                message: "Success!",
+                data: updatedPost,
+            });
+        }
+    );
+};
+
+const destroy = (req, res) => {
+    db.Post.findbyIdAndDelete(req.params.id, (err, deletedPost) => {
+        if (err)
+            return res.status(400).json({
+                message: "Status 400 Error",
+                error: err,
+            });
+        return res.status(200).json({
+            message: "success!",
+            data: deletedPost,
+        });
+    });
+};
+
 
 module.exports = {
     index,
+    show,
+    create,
+    update,
+    destroy,
 }
--- poems-backend/routes/post.js
+++ poems-backend/routes/post.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postCtrl = require("../controllers/post");
 
-router.get("/", postCtrl.index);
+router.get("/", postCtrl.index); //all posts
+router.get("/:id", postCtrl.show); //one post
+router.get("/", postCtrl.create);
+router.get("/:id", postCtrl.update);
+router.get("/:id", postCtrl.destroy);
 
 module.exports = router;
